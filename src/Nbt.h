c250ed5c0b7cf4896b1b95ef4bde8b1ec17a086b
simon <simon.kahre@gmail.com>
2023-10-27 21:51:24 +0200

    LEGAL

--- src/Nbt.h
+++ src/Nbt.h
@@ -1,3 +1,16 @@
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <phk@FreeBSD.ORG> wrote this file.  As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp
+ * ----------------------------------------------------------------------------
+ */
+
+/*
+ * NOTICE: Some functions may be modifyed or copied from this repo https://github.dev/chmod222/cNBT.
+ */
+
 #pragma once
 
 #include <stdint.h>
