4be3a8583c9590632fbfc05e341df1e34a77455c
Admin Else <74665496+admin-else@users.noreply.github.com>
2024-02-28 12:59:54 +0100

    mutilverse work

--- src/packet_types.h
+++ src/packet_types.h
@@ -5,335 +5,109 @@
 // that the reason for the existence of this file
 
 // CGSS: packet_types
-typedef struct {
-  int protocole_version;
-  char *server_addr;
-  unsigned short server_port;
-  int next_state;
-} C2S_handshake_handshake_packet;
+typedef struct {int protocole_version;int next_state;unsigned short server_port;char *server_addr;} C2S_handshake_handshake_packet;
 
-typedef struct {
-  char *response;
-} S2C_status_response_packet;
+typedef struct {char *response;} S2C_status_response_packet;
 
-typedef struct {
-  long payload;
-} S2C_status_pong_packet;
+typedef struct {long payload;} S2C_status_pong_packet;
 
-typedef struct {
-  long payload;
-} C2S_status_ping_packet;
+typedef struct {long payload;} C2S_status_ping_packet;
 
-typedef struct {
-  char *reason;
-} S2C_login_disconnect_packet;
+typedef struct {char *reason;} S2C_login_disconnect_packet;
 
-typedef struct {
-  char *server_id;
-  cmc_buffer *public_key;
-  cmc_buffer *verify_token;
-} S2C_login_encryption_request_packet;
+typedef struct {cmc_buffer *verify_token;char *server_id;cmc_buffer *public_key;} S2C_login_encryption_request_packet;
 
-typedef struct {
-  char *uuid;
-  char *name;
-} S2C_login_success_packet;
+typedef struct {char *name;char *uuid;} S2C_login_success_packet;
 
-typedef struct {
-  int threshold;
-} S2C_login_set_compression_packet;
+typedef struct {int threshold;} S2C_login_set_compression_packet;
 
-typedef struct {
-  char *name;
-} C2S_login_start_packet;
+typedef struct {char *name;} C2S_login_start_packet;
 
-typedef struct {
-  cmc_buffer *shared_secret;
-  cmc_buffer *verify_token;
-} C2S_login_encryption_response_packet;
+typedef struct {cmc_buffer *verify_token;cmc_buffer *shared_secret;} C2S_login_encryption_response_packet;
 
-typedef struct {
-  int keep_alive_id;
-} S2C_play_keep_alive_packet;
+typedef struct {int keep_alive_id;} S2C_play_keep_alive_packet;
 
-typedef struct {
-  int entity_id;
-  unsigned char gamemode;
-  char dimension;
-  unsigned char difficulty;
-  unsigned char max_players;
-  char *level_type;
-  bool reduced_debug_info;
-} S2C_play_join_game_packet;
+typedef struct {unsigned char max_players;char dimension;unsigned char difficulty;bool reduced_debug_info;char *level_type;unsigned char gamemode;int entity_id;} S2C_play_join_game_packet;
 
-typedef struct {
-  char *message;
-  char position;
-} S2C_play_chat_message_packet;
+typedef struct {char *message;char position;} S2C_play_chat_message_packet;
 
-typedef struct {
-  long world_age;
-  long time_of_day;
-} S2C_play_time_update_packet;
+typedef struct {long world_age;long time_of_day;} S2C_play_time_update_packet;
 
-typedef struct {
-  int entity_id;
-  short slot;
-  cmc_slot *item;
-} S2C_play_entity_equipment_packet;
+typedef struct {cmc_slot *item;int entity_id;short slot;} S2C_play_entity_equipment_packet;
 
-typedef struct {
-  cmc_block_pos location;
-} S2C_play_spawn_position_packet;
+typedef struct {cmc_block_pos location;} S2C_play_spawn_position_packet;
 
-typedef struct {
-  float health;
-  int food;
-  float food_saturation;
-} S2C_play_update_health_packet;
+typedef struct {int food;float food_saturation;float health;} S2C_play_update_health_packet;
 
-typedef struct {
-  int dimesion;
-  unsigned char difficulty;
-  unsigned char gamemode;
-  char *level_type;
-} S2C_play_respawn_packet;
+typedef struct {char *level_type;int dimesion;unsigned char difficulty;unsigned char gamemode;} S2C_play_respawn_packet;
 
-typedef struct {
-  double x;
-  double y;
-  double z;
-  float yaw;
-  float pitch;
-  unsigned char flags;
-} S2C_play_player_look_and_position_packet;
+typedef struct {float yaw;unsigned char flags;float pitch;double y;double x;double z;} S2C_play_player_look_and_position_packet;
 
-typedef struct {
-  char slot;
-} S2C_play_held_item_change_packet;
+typedef struct {char slot;} S2C_play_held_item_change_packet;
 
-typedef struct {
-  int entity_id;
-  cmc_block_pos location;
-} S2C_play_use_bed_packet;
+typedef struct {int entity_id;cmc_block_pos location;} S2C_play_use_bed_packet;
 
-typedef struct {
-  int entity_id;
-  unsigned char animation;
-} S2C_play_animation_packet;
+typedef struct {int entity_id;unsigned char animation;} S2C_play_animation_packet;
 
-typedef struct {
-  int entity_id;
-  unsigned long long uuid;
-  int x;
-  int y;
-  int z;
-  unsigned char yaw;
-  unsigned char pitch;
-  short current_item;
-  cmc_entity_metadata meta_data;
-} S2C_play_spawn_player_packet;
+typedef struct {cmc_entity_metadata meta_data;unsigned char pitch;int x;int z;unsigned char yaw;short current_item;int entity_id;int y;unsigned long long uuid;} S2C_play_spawn_player_packet;
 
-typedef struct {
-  int collected_entity_id;
-  int collector_entity_id;
-} S2C_play_collect_item_packet;
+typedef struct {int collected_entity_id;int collector_entity_id;} S2C_play_collect_item_packet;
 
-typedef struct {
-  int entity_id;
-  unsigned char type;
-  int x;
-  int y;
-  int z;
-  unsigned char yaw;
-  unsigned char pitch;
-  unsigned char head_pitch;
-  short x_vel;
-  short y_vel;
-  short z_vel;
-  cmc_entity_metadata meta_data;
-} S2C_play_spawn_mob_packet;
+typedef struct {short x_vel;cmc_entity_metadata meta_data;unsigned char pitch;int x;int z;unsigned char yaw;short z_vel;unsigned char type;int entity_id;short y_vel;int y;unsigned char head_pitch;} S2C_play_spawn_mob_packet;
 
-typedef struct {
-  int entity_id;
-  char *title;
-  cmc_block_pos location;
-  unsigned char direction;
-} S2C_play_spawn_painting_packet;
+typedef struct {int entity_id;unsigned char direction;char *title;cmc_block_pos location;} S2C_play_spawn_painting_packet;
 
-typedef struct {
-  int entity_id;
-  int x;
-  int y;
-  int z;
-  short count;
-} S2C_play_spawn_experience_orb_packet;
+typedef struct {int x;int z;int entity_id;short count;int y;} S2C_play_spawn_experience_orb_packet;
 
-typedef struct {
-  int entity_id;
-  short x_vel;
-  short y_vel;
-  short z_vel;
-} S2C_play_entity_velocity_packet;
+typedef struct {short x_vel;int entity_id;short y_vel;short z_vel;} S2C_play_entity_velocity_packet;
 
-typedef struct {
-  int entity_id;
-} S2C_play_entity_packet;
+typedef struct {int entity_id;} S2C_play_entity_packet;
 
-typedef struct {
-  int entity_id;
-  char delta_x;
-  char delta_y;
-  char delta_z;
-  bool on_ground;
-} S2C_play_entity_relative_move_packet;
+typedef struct {bool on_ground;char delta_z;char delta_y;char delta_x;int entity_id;} S2C_play_entity_relative_move_packet;
 
-typedef struct {
-  int entity_id;
-  unsigned char yaw;
-  unsigned char pitch;
-  bool on_ground;
-} S2C_play_entity_look_packet;
+typedef struct {unsigned char yaw;unsigned char pitch;int entity_id;bool on_ground;} S2C_play_entity_look_packet;
 
-typedef struct {
-  int entity_id;
-  char delta_x;
-  char delta_y;
-  char delta_z;
-  unsigned char yaw;
-  unsigned char pitch;
-  bool on_ground;
-} S2C_play_entity_look_and_relative_move_packet;
+typedef struct {bool on_ground;char delta_z;char delta_y;unsigned char yaw;char delta_x;int entity_id;unsigned char pitch;} S2C_play_entity_look_and_relative_move_packet;
 
-typedef struct {
-  int entity_id;
-  int x;
-  int y;
-  int z;
-  unsigned char yaw;
-  unsigned char pitch;
-  bool on_ground;
-} S2C_play_entity_teleport_packet;
+typedef struct {bool on_ground;unsigned char pitch;int x;int z;unsigned char yaw;int entity_id;int y;} S2C_play_entity_teleport_packet;
 
-typedef struct {
-  int entity_id;
-  unsigned char head_yaw;
-} S2C_play_entity_head_look_packet;
+typedef struct {int entity_id;unsigned char head_yaw;} S2C_play_entity_head_look_packet;
 
-typedef struct {
-  int entity_id;
-  char entity_status;
-} S2C_play_entity_status_packet;
+typedef struct {char entity_status;int entity_id;} S2C_play_entity_status_packet;
 
-typedef struct {
-  int entity_id;
-  int vehicle_id;
-  bool leash;
-} S2C_play_attach_entity_packet;
+typedef struct {bool leash;int vehicle_id;int entity_id;} S2C_play_attach_entity_packet;
 
-typedef struct {
-  int entity_id;
-  cmc_entity_metadata meta_data;
-} S2C_play_entity_metadata_packet;
+typedef struct {int entity_id;cmc_entity_metadata meta_data;} S2C_play_entity_metadata_packet;
 
-typedef struct {
-  int entity_id;
-  char effect_id;
-  char amplifier;
-  int duration;
-  bool hide_particles;
-} S2C_play_entity_effect_packet;
+typedef struct {char effect_id;bool hide_particles;char amplifier;int entity_id;int duration;} S2C_play_entity_effect_packet;
 
-typedef struct {
-  int entity_id;
-  char effect_id;
-} S2C_play_remove_entity_effect_packet;
+typedef struct {char effect_id;int entity_id;} S2C_play_remove_entity_effect_packet;
 
-typedef struct {
-  float experience_bar;
-  int level;
-  int total_experience;
-} S2C_play_set_experience_packet;
+typedef struct {int level;float experience_bar;int total_experience;} S2C_play_set_experience_packet;
 
-typedef struct {
-  int chunk_x;
-  int chunk_z;
-  bool ground_up_continuous;
-  unsigned short primary_bitmask;
-  cmc_buffer *chunk;
-} S2C_play_chunk_data_packet;
+typedef struct {cmc_buffer *chunk;unsigned short primary_bitmask;int chunk_x;bool ground_up_continuous;int chunk_z;} S2C_play_chunk_data_packet;
 
-typedef struct {
-  cmc_block_pos location;
-  int block_id;
-} S2C_play_block_change_packet;
+typedef struct {int block_id;cmc_block_pos location;} S2C_play_block_change_packet;
 
-typedef struct {
-  cmc_block_pos location;
-  unsigned char block_data_1;
-  unsigned char block_data_2;
-  int block_type;
-} S2C_play_block_action_packet;
+typedef struct {int block_type;unsigned char block_data_1;unsigned char block_data_2;cmc_block_pos location;} S2C_play_block_action_packet;
 
-typedef struct {
-  int entity_id;
-  cmc_block_pos location;
-  char destroy_stage;
-} S2C_play_block_break_animation_packet;
+typedef struct {int entity_id;char destroy_stage;cmc_block_pos location;} S2C_play_block_break_animation_packet;
 
-typedef struct {
-  int effect_id;
-  cmc_block_pos location;
-  int data;
-  bool d;
-  int particle_id;
-  bool long_distances;
-  float x;
-  float y;
-  float z;
-  float x_offset;
-  float y_offset;
-  float z_offset;
-  float particle_data;
-  int particle_count;
-  int sable_relative_volume;
-} S2C_play_effect_packet;
+typedef struct {int effect_id;int particle_id;int sable_relative_volume;float x_offset;int data;cmc_block_pos location;float particle_data;int particle_count;float y_offset;bool long_distances;float z;bool d;float x;float y;float z_offset;} S2C_play_effect_packet;
 
-typedef struct {
-  char *sound_name;
-  int x;
-  int y;
-  int z;
-  float volume;
-  unsigned char pitch;
-} S2C_play_sound_effect_packet;
+typedef struct {float volume;int x;unsigned char pitch;int z;char *sound_name;int y;} S2C_play_sound_effect_packet;
 
-typedef struct {
-  unsigned char reason;
-  float value;
-} S2C_play_change_game_state_packet;
+typedef struct {float value;unsigned char reason;} S2C_play_change_game_state_packet;
 
-typedef struct {
-  char flags;
-  float flying_speed;
-  float fov_modifier;
-} S2C_play_player_abilities_packet;
+typedef struct {float fov_modifier;char flags;float flying_speed;} S2C_play_player_abilities_packet;
 
-typedef struct {
-  char *channel;
-  cmc_buffer *data;
-} S2C_play_plugin_message_packet;
+typedef struct {cmc_buffer *data;char *channel;} S2C_play_plugin_message_packet;
 
-typedef struct {
-  char *reason;
-} S2C_play_disconnect_packet;
+typedef struct {char *reason;} S2C_play_disconnect_packet;
 
-typedef struct {
-  unsigned char difficulty;
-} S2C_play_change_difficulty_packet;
+typedef struct {unsigned char difficulty;} S2C_play_change_difficulty_packet;
 
-typedef struct {
-  int keep_alive_id;
-} C2S_play_keep_alive_packet;
+typedef struct {int keep_alive_id;} C2S_play_keep_alive_packet;
+
 
 // CGSE: packet_types
--- src/packets.h
+++ src/packets.h
@@ -9,75 +9,9 @@
 char *packet_id_to_string(int packet_id, cmc_conn_state state,
                           packet_direction direction);
 
-// CGSS: packet_ids
-enum packetids_C2S_handshake {
-  CMC_PACKETID_C2S_HANDSHAKE_HANDSHAKE = 0x00,
-};
-enum packetids_S2C_status {
-  CMC_PACKETID_S2C_STATUS_RESPONSE = 0x00,
-  CMC_PACKETID_S2C_STATUS_PONG = 0x01,
-};
-enum packetids_C2S_status {
-  CMC_PACKETID_C2S_STATUS_REQUEST = 0x00,
-  CMC_PACKETID_C2S_STATUS_PING = 0x01,
-};
-enum packetids_S2C_login {
-  CMC_PACKETID_S2C_LOGIN_DISCONNECT = 0x00,
-  CMC_PACKETID_S2C_LOGIN_ENCRYPTION_REQUEST = 0x01,
-  CMC_PACKETID_S2C_LOGIN_SUCCESS = 0x02,
-  CMC_PACKETID_S2C_LOGIN_SET_COMPRESSION = 0x03,
-};
-enum packetids_C2S_login {
-  CMC_PACKETID_C2S_LOGIN_START = 0x00,
-  CMC_PACKETID_C2S_LOGIN_ENCRYPTION_RESPONSE = 0x01,
-};
-enum packetids_S2C_play {
-  CMC_PACKETID_S2C_PLAY_KEEP_ALIVE = 0x00,
-  CMC_PACKETID_S2C_PLAY_JOIN_GAME = 0x01,
-  CMC_PACKETID_S2C_PLAY_CHAT_MESSAGE = 0x02,
-  CMC_PACKETID_S2C_PLAY_TIME_UPDATE = 0x03,
-  CMC_PACKETID_S2C_PLAY_ENTITY_EQUIPMENT = 0x04,
-  CMC_PACKETID_S2C_PLAY_SPAWN_POSITION = 0x05,
-  CMC_PACKETID_S2C_PLAY_UPDATE_HEALTH = 0x06,
-  CMC_PACKETID_S2C_PLAY_RESPAWN = 0x07,
-  CMC_PACKETID_S2C_PLAY_PLAYER_LOOK_AND_POSITION = 0x08,
-  CMC_PACKETID_S2C_PLAY_HELD_ITEM_CHANGE = 0x09,
-  CMC_PACKETID_S2C_PLAY_USE_BED = 0x0A,
-  CMC_PACKETID_S2C_PLAY_ANIMATION = 0x0B,
-  CMC_PACKETID_S2C_PLAY_SPAWN_PLAYER = 0x0C,
-  CMC_PACKETID_S2C_PLAY_COLLECT_ITEM = 0x0D,
-  CMC_PACKETID_S2C_PLAY_SPAWN_MOB = 0x0F,
-  CMC_PACKETID_S2C_PLAY_SPAWN_PAINTING = 0x10,
-  CMC_PACKETID_S2C_PLAY_SPAWN_EXPERIENCE_ORB = 0x11,
-  CMC_PACKETID_S2C_PLAY_ENTITY_VELOCITY = 0x12,
-  CMC_PACKETID_S2C_PLAY_ENTITY = 0x14,
-  CMC_PACKETID_S2C_PLAY_ENTITY_RELATIVE_MOVE = 0x15,
-  CMC_PACKETID_S2C_PLAY_ENTITY_LOOK = 0x16,
-  CMC_PACKETID_S2C_PLAY_ENTITY_LOOK_AND_RELATIVE_MOVE = 0x17,
-  CMC_PACKETID_S2C_PLAY_ENTITY_TELEPORT = 0x18,
-  CMC_PACKETID_S2C_PLAY_ENTITY_HEAD_LOOK = 0x19,
-  CMC_PACKETID_S2C_PLAY_ENTITY_STATUS = 0x1A,
-  CMC_PACKETID_S2C_PLAY_ATTACH_ENTITY = 0x1B,
-  CMC_PACKETID_S2C_PLAY_ENTITY_METADATA = 0x1C,
-  CMC_PACKETID_S2C_PLAY_ENTITY_EFFECT = 0x1D,
-  CMC_PACKETID_S2C_PLAY_REMOVE_ENTITY_EFFECT = 0x1E,
-  CMC_PACKETID_S2C_PLAY_SET_EXPERIENCE = 0x1F,
-  CMC_PACKETID_S2C_PLAY_CHUNK_DATA = 0x21,
-  CMC_PACKETID_S2C_PLAY_BLOCK_CHANGE = 0x23,
-  CMC_PACKETID_S2C_PLAY_BLOCK_ACTION = 0x24,
-  CMC_PACKETID_S2C_PLAY_BLOCK_BREAK_ANIMATION = 0x25,
-  CMC_PACKETID_S2C_PLAY_EFFECT = 0x28,
-  CMC_PACKETID_S2C_PLAY_SOUND_EFFECT = 0x29,
-  CMC_PACKETID_S2C_PLAY_CHANGE_GAME_STATE = 0x2B,
-  CMC_PACKETID_S2C_PLAY_PLAYER_ABILITIES = 0x39,
-  CMC_PACKETID_S2C_PLAY_PLUGIN_MESSAGE = 0x3F,
-  CMC_PACKETID_S2C_PLAY_DISCONNECT = 0x40,
-  CMC_PACKETID_S2C_PLAY_CHANGE_DIFFICULTY = 0x41,
-};
-enum packetids_C2S_play {
-  CMC_PACKETID_C2S_PLAY_KEEP_ALIVE = 0x00,
-};
-// CGSE: packet_ids
+// CGSS: packet_name_id_define
+typedef enum {CMC_S2C_PLAY_ENTITY_LOOK_AND_RELATIVE_MOVE_NAME_ID,CMC_S2C_LOGIN_SET_COMPRESSION_NAME_ID,CMC_S2C_PLAY_SPAWN_MOB_NAME_ID,CMC_S2C_PLAY_SPAWN_PAINTING_NAME_ID,CMC_S2C_PLAY_KEEP_ALIVE_NAME_ID,CMC_S2C_PLAY_ENTITY_TELEPORT_NAME_ID,CMC_S2C_PLAY_BLOCK_BREAK_ANIMATION_NAME_ID,CMC_S2C_PLAY_UPDATE_HEALTH_NAME_ID,CMC_S2C_PLAY_ENTITY_HEAD_LOOK_NAME_ID,CMC_S2C_PLAY_ATTACH_ENTITY_NAME_ID,CMC_S2C_PLAY_SET_EXPERIENCE_NAME_ID,CMC_S2C_PLAY_ANIMATION_NAME_ID,CMC_C2S_STATUS_PING_NAME_ID,CMC_S2C_PLAY_COLLECT_ITEM_NAME_ID,CMC_S2C_PLAY_PLUGIN_MESSAGE_NAME_ID,CMC_C2S_STATUS_REQUEST_NAME_ID,CMC_S2C_PLAY_CHUNK_DATA_NAME_ID,CMC_S2C_PLAY_TIME_UPDATE_NAME_ID,CMC_S2C_PLAY_CHANGE_GAME_STATE_NAME_ID,CMC_S2C_PLAY_PLAYER_ABILITIES_NAME_ID,CMC_S2C_PLAY_BLOCK_CHANGE_NAME_ID,CMC_S2C_PLAY_SPAWN_POSITION_NAME_ID,CMC_C2S_HANDSHAKE_HANDSHAKE_NAME_ID,CMC_S2C_PLAY_SOUND_EFFECT_NAME_ID,CMC_S2C_PLAY_JOIN_GAME_NAME_ID,CMC_S2C_PLAY_BLOCK_ACTION_NAME_ID,CMC_S2C_STATUS_RESPONSE_NAME_ID,CMC_S2C_PLAY_ENTITY_RELATIVE_MOVE_NAME_ID,CMC_S2C_PLAY_EFFECT_NAME_ID,CMC_S2C_PLAY_PLAYER_LOOK_AND_POSITION_NAME_ID,CMC_S2C_LOGIN_ENCRYPTION_REQUEST_NAME_ID,CMC_S2C_PLAY_ENTITY_EQUIPMENT_NAME_ID,CMC_S2C_PLAY_ENTITY_METADATA_NAME_ID,CMC_S2C_PLAY_USE_BED_NAME_ID,CMC_S2C_PLAY_ENTITY_VELOCITY_NAME_ID,CMC_S2C_PLAY_HELD_ITEM_CHANGE_NAME_ID,CMC_S2C_PLAY_REMOVE_ENTITY_EFFECT_NAME_ID,CMC_S2C_PLAY_ENTITY_NAME_ID,CMC_S2C_PLAY_RESPAWN_NAME_ID,CMC_C2S_PLAY_KEEP_ALIVE_NAME_ID,CMC_S2C_LOGIN_SUCCESS_NAME_ID,CMC_S2C_PLAY_DISCONNECT_NAME_ID,CMC_S2C_PLAY_CHANGE_DIFFICULTY_NAME_ID,CMC_S2C_PLAY_CHAT_MESSAGE_NAME_ID,CMC_S2C_PLAY_ENTITY_EFFECT_NAME_ID,CMC_S2C_PLAY_SPAWN_EXPERIENCE_ORB_NAME_ID,CMC_S2C_PLAY_SPAWN_PLAYER_NAME_ID,CMC_C2S_LOGIN_START_NAME_ID,CMC_S2C_LOGIN_DISCONNECT_NAME_ID,CMC_C2S_LOGIN_ENCRYPTION_RESPONSE_NAME_ID,CMC_S2C_PLAY_ENTITY_STATUS_NAME_ID,CMC_S2C_STATUS_PONG_NAME_ID,CMC_S2C_PLAY_ENTITY_LOOK_NAME_ID,}cmc_packet_name_id;
+// CGSE: packet_name_id_define
 
 // CGSS: send_methods_h
 void cmc_send_C2S_handshake_handshake_packet(cmc_conn *conn, C2S_handshake_handshake_packet *packet);void cmc_send_S2C_status_response_packet(cmc_conn *conn, S2C_status_response_packet *packet);void cmc_send_S2C_status_pong_packet(cmc_conn *conn, S2C_status_pong_packet *packet);void cmc_send_C2S_status_request_packet(cmc_conn *conn);void cmc_send_C2S_status_ping_packet(cmc_conn *conn, C2S_status_ping_packet *packet);void cmc_send_S2C_login_disconnect_packet(cmc_conn *conn, S2C_login_disconnect_packet *packet);void cmc_send_S2C_login_encryption_request_packet(cmc_conn *conn, S2C_login_encryption_request_packet *packet);void cmc_send_S2C_login_success_packet(cmc_conn *conn, S2C_login_success_packet *packet);void cmc_send_S2C_login_set_compression_packet(cmc_conn *conn, S2C_login_set_compression_packet *packet);void cmc_send_C2S_login_start_packet(cmc_conn *conn, C2S_login_start_packet *packet);void cmc_send_C2S_login_encryption_response_packet(cmc_conn *conn, C2S_login_encryption_response_packet *packet);void cmc_send_S2C_play_keep_alive_packet(cmc_conn *conn, S2C_play_keep_alive_packet *packet);void cmc_send_S2C_play_join_game_packet(cmc_conn *conn, S2C_play_join_game_packet *packet);void cmc_send_S2C_play_chat_message_packet(cmc_conn *conn, S2C_play_chat_message_packet *packet);void cmc_send_S2C_play_time_update_packet(cmc_conn *conn, S2C_play_time_update_packet *packet);void cmc_send_S2C_play_entity_equipment_packet(cmc_conn *conn, S2C_play_entity_equipment_packet *packet);void cmc_send_S2C_play_spawn_position_packet(cmc_conn *conn, S2C_play_spawn_position_packet *packet);void cmc_send_S2C_play_update_health_packet(cmc_conn *conn, S2C_play_update_health_packet *packet);void cmc_send_S2C_play_respawn_packet(cmc_conn *conn, S2C_play_respawn_packet *packet);void cmc_send_S2C_play_player_look_and_position_packet(cmc_conn *conn, S2C_play_player_look_and_position_packet *packet);void cmc_send_S2C_play_held_item_change_packet(cmc_conn *conn, S2C_play_held_item_change_packet *packet);void cmc_send_S2C_play_use_bed_packet(cmc_conn *conn, S2C_play_use_bed_packet *packet);void cmc_send_S2C_play_animation_packet(cmc_conn *conn, S2C_play_animation_packet *packet);void cmc_send_S2C_play_spawn_player_packet(cmc_conn *conn, S2C_play_spawn_player_packet *packet);void cmc_send_S2C_play_collect_item_packet(cmc_conn *conn, S2C_play_collect_item_packet *packet);void cmc_send_S2C_play_spawn_mob_packet(cmc_conn *conn, S2C_play_spawn_mob_packet *packet);void cmc_send_S2C_play_spawn_painting_packet(cmc_conn *conn, S2C_play_spawn_painting_packet *packet);void cmc_send_S2C_play_spawn_experience_orb_packet(cmc_conn *conn, S2C_play_spawn_experience_orb_packet *packet);void cmc_send_S2C_play_entity_velocity_packet(cmc_conn *conn, S2C_play_entity_velocity_packet *packet);void cmc_send_S2C_play_entity_packet(cmc_conn *conn, S2C_play_entity_packet *packet);void cmc_send_S2C_play_entity_relative_move_packet(cmc_conn *conn, S2C_play_entity_relative_move_packet *packet);void cmc_send_S2C_play_entity_look_packet(cmc_conn *conn, S2C_play_entity_look_packet *packet);void cmc_send_S2C_play_entity_look_and_relative_move_packet(cmc_conn *conn, S2C_play_entity_look_and_relative_move_packet *packet);void cmc_send_S2C_play_entity_teleport_packet(cmc_conn *conn, S2C_play_entity_teleport_packet *packet);void cmc_send_S2C_play_entity_head_look_packet(cmc_conn *conn, S2C_play_entity_head_look_packet *packet);void cmc_send_S2C_play_entity_status_packet(cmc_conn *conn, S2C_play_entity_status_packet *packet);void cmc_send_S2C_play_attach_entity_packet(cmc_conn *conn, S2C_play_attach_entity_packet *packet);void cmc_send_S2C_play_entity_metadata_packet(cmc_conn *conn, S2C_play_entity_metadata_packet *packet);void cmc_send_S2C_play_entity_effect_packet(cmc_conn *conn, S2C_play_entity_effect_packet *packet);void cmc_send_S2C_play_remove_entity_effect_packet(cmc_conn *conn, S2C_play_remove_entity_effect_packet *packet);void cmc_send_S2C_play_set_experience_packet(cmc_conn *conn, S2C_play_set_experience_packet *packet);void cmc_send_S2C_play_chunk_data_packet(cmc_conn *conn, S2C_play_chunk_data_packet *packet);void cmc_send_S2C_play_block_change_packet(cmc_conn *conn, S2C_play_block_change_packet *packet);void cmc_send_S2C_play_block_action_packet(cmc_conn *conn, S2C_play_block_action_packet *packet);void cmc_send_S2C_play_block_break_animation_packet(cmc_conn *conn, S2C_play_block_break_animation_packet *packet);void cmc_send_S2C_play_effect_packet(cmc_conn *conn, S2C_play_effect_packet *packet);void cmc_send_S2C_play_sound_effect_packet(cmc_conn *conn, S2C_play_sound_effect_packet *packet);void cmc_send_S2C_play_change_game_state_packet(cmc_conn *conn, S2C_play_change_game_state_packet *packet);void cmc_send_S2C_play_player_abilities_packet(cmc_conn *conn, S2C_play_player_abilities_packet *packet);void cmc_send_S2C_play_plugin_message_packet(cmc_conn *conn, S2C_play_plugin_message_packet *packet);void cmc_send_S2C_play_disconnect_packet(cmc_conn *conn, S2C_play_disconnect_packet *packet);void cmc_send_S2C_play_change_difficulty_packet(cmc_conn *conn, S2C_play_change_difficulty_packet *packet);void cmc_send_C2S_play_keep_alive_packet(cmc_conn *conn, C2S_play_keep_alive_packet *packet);
