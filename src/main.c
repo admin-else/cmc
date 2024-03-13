e85cb0304476d0f9d6aa2c5ebd759ef2083a2ece
Admin Else <74665496+admin-else@users.noreply.github.com>
2024-03-13 12:58:44 +0100

    testing...

--- src/main.c
+++ src/main.c
@@ -9,43 +9,38 @@ enum cmc_err cmc_err = ERR_NO;
 
 int main() {
   cmc_conn conn = cmc_conn_init(765);
-  cmc_conn_login(&conn);
-  if (cmc_err != ERR_NO) {
-    printf("%s\n", cmc_err_as_str(cmc_err));
-    return 1;
-  }
+  
+  conn.sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  ERR_IF_NOT(conn.sockfd, ERR_SOCKET, return 1;);
+
+  ERR_IF_LESS_THAN_ZERO(
+      connect(conn.sockfd, (struct sockaddr *)&conn.addr, sizeof(conn.addr)),
+      ERR_CONNETING, return 1;);
+
+  C2S_handshake_handshake_packet handshake =
+      (C2S_handshake_handshake_packet){.next_state = CMC_CONN_STATE_LOGIN,
+                                       .server_port = 25565,
+                                       .server_addr = "127.0.0.1",
+                                       .protocole_version = conn.protocol_version};
+  C2S_login_start_packet login_start = {.name = "Botzo",
+                                        .uuid = {.lower = 0, .upper = 0}};
+  
+  conn.state = CMC_CONN_STATE_HANDSHAKE;
+  
+  ERR_ABLE(cmc_send_C2S_handshake_handshake_packet(&conn, &handshake), goto err_close_conn;);
+  ERR_ABLE(cmc_send_C2S_login_start_packet(&conn, &login_start), goto err_close_conn;);
+  
+  conn.state = CMC_CONN_STATE_LOGIN;
+  
   while (conn.state != CMC_CONN_STATE_OFFLINE) {
-    cmc_buffer *raw_packet = ERR_ABLE(cmc_conn_recive_packet(&conn), break;);
+    cmc_buffer *raw_packet = ERR_ABLE(cmc_conn_recive_packet(&conn), goto err_close_conn;);
     int packet_id = cmc_buffer_unpack_varint(raw_packet);
-    cmc_packet_name_id pnid = cmc_packet_id_to_packet_name_id(
-        packet_id, conn.state, CMC_DIRECTION_S2C, 765);
-    switch (pnid) {
-    case CMC_S2C_PLAY_KEEP_ALIVE_NAME_ID: {
-      puts("keepalive");
-      S2C_play_keep_alive_packet keep_alive_sever =
-          unpack_S2C_play_keep_alive_packet(raw_packet);
-      C2S_play_keep_alive_packet keep_alive_client = {
-          .keep_alive_id = keep_alive_sever.keep_alive_id};
-      cmc_send_C2S_play_keep_alive_packet(&conn, &keep_alive_client);
-      cmc_free_C2S_play_keep_alive_packet(&keep_alive_client);
-      cmc_free_S2C_play_keep_alive_packet(&keep_alive_sever);
-      break;
-    }
-    default: {
-      printf("packet %s, packet_id %d\n", cmc_packet_name_id_string(pnid),
-             packet_id);
-    }
-    }
+    cmc_packet_id_to_packet_name_id(packet_id, conn.state, CMC_DIRECTION_S2C, conn.protocol_version);
     cmc_buffer_free(raw_packet);
-    if (cmc_err) {
-      conn.state = CMC_CONN_STATE_OFFLINE;
-      printf("error occured %s\n", cmc_err_as_str(cmc_err));
-    }
   }
-  cmc_conn_close(&conn);
-  if (cmc_err) {
-    conn.state = CMC_CONN_STATE_OFFLINE;
-    printf("error occured %s\n", cmc_err_as_str(cmc_err));
+err_close_conn:
+  if (conn.state != CMC_CONN_STATE_OFFLINE) {
+    cmc_conn_close(&conn);
   }
   return 0;
 }
