a91ab4a0650dd2cde6c7b7fbebdeeaefffd761e8
admin-else <adman.else@gmail.com>
2024-06-22 16:52:12 +0200

    move errable macros to public header for convenice

--- src/err_macros.h
+++ src/err_macros.h
@@ -1,12 +1,10 @@
 #pragma once
+#include <cmc/err.h>
 
 /*
 ALL errors if they are not in the ctx object are in cmc_err_extra *err
 to ensure these macros work
-*/
 
-/*
-A (kinda)documentation for the error handlers
 CMC_ERR???
 
 A(bstaract): parent of all other types you can also use it in weird situations
@@ -22,12 +20,6 @@ B(uff): like conn but into buffer
     action;                                                                    \
   } while (0)
 
-#define CMC_ERRA_ABLE(code, err_var, action)                                   \
-  code;                                                                        \
-  if (err_var) {                                                               \
-    action;                                                                    \
-  }
-
 #define CMC_ERRA_IF(condition, err_var, err_type, action)                      \
   if (condition) {                                                             \
     CMC_ERRA(err_type, action, err_var);                                       \
@@ -35,10 +27,8 @@ B(uff): like conn but into buffer
 
 #define CMC_ERR(err_type, action) CMC_ERRA(err_type, action, *err)
 #define CMC_ERRR(err_type) CMC_ERR(err_type, return err_type)
-#define CMC_ERRRB_ABLE(code)                                                   \
-  CMC_ERRA_ABLE(code, buff->err.err, return buff->err.err;)
+
 #define CMC_ERRB(err_type, action) CMC_ERRA(err_type, action, buff->err)
-#define CMC_ERRB_ABLE(code, action) CMC_ERRA_ABLE(code, buff->err.err, action)
 
 #define CMC_ERRB_IF(condition, err_type, action) CMC_ERRA_IF(condition, buff->err, err_type, action)
 
@@ -50,6 +40,4 @@ B(uff): like conn but into buffer
 #define CMC_ERRC_IF(condition, err_type, action)                               \
   CMC_ERRA_IF(condition, conn->err, err_type, action)
 
-#define CMC_ERRC_ABLE(code, action) CMC_ERRA_ABLE(code, conn->err.err, action)
 #define CMC_ERRC(code, action) CMC_ERRA(code, conn->err.err, action)
-#define CMC_ERR_ABLE(code, action) CMC_ERRA_ABLE(code, err->err, action)
