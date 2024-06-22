c78c55fa8bb069d99f0112fb33198e993efb3743
admin-else <adman.else@gmail.com>
2024-06-22 13:12:38 +0200

    updated err_macros to use the erra macros more

--- src/err_macros.h
+++ src/err_macros.h
@@ -28,49 +28,24 @@ B(uff): like conn but into buffer
     action;                                                                    \
   }
 
-#define CMC_ERRA_ABLE(code, err_var, action)                                   \
-  code;                                                                        \
-  if (err_var) {                                                               \
-    action;                                                                    \
+#define CMC_ERRA_IF(condition, err_var, err_type, action)                      \
+  if (condition) {                                                             \
+    CMC_ERRA(err_type, action, err_var);                                       \
   }
 
 #define CMC_ERR(err_type, action) CMC_ERRA(err_type, action, *err)
 #define CMC_ERRR(err_type) CMC_ERR(err_type, return err_type)
 #define CMC_ERRRB_ABLE(code)                                                   \
-  code;                                                                        \
-  if (buff->err.err)                                                           \
-    return buff->err.err;
-
-#define CMC_ERRB(err_type, action)                                             \
-  do {                                                                         \
-    buff->err =                                                                \
-        (cmc_err_extra){.file = __FILE__, .line = __LINE__, .err = err_type};  \
-    action;                                                                    \
-  } while (0)
-
-#define CMC_ERRB_ABLE(code, action)                                            \
-  code;                                                                        \
-  if (buff->err.err) {                                                         \
-    action;                                                                    \
-  }
+  CMC_ERRA_ABLE(code, buff->err.err, return buff->err.err;)
+#define CMC_ERRB(err_type, action) CMC_ERRA(err_type, action, buff->err)
+#define CMC_ERRB_ABLE(code, action) CMC_ERRA_ABLE(code, buff->err.err, action)
 
 #define CMC_ERRB_IF(condition, err_type, action)                               \
   if (condition) {                                                             \
     CMC_ERRB(err_type, action);                                                \
   }
 
-#define CMC_ERRRB(err_type)                                                    \
-  do {                                                                         \
-    buff->err =                                                                \
-        (cmc_err_extra){.file = __FILE__, .line = __LINE__, .err = err_type};  \
-    return err_type;                                                           \
-  } while (0)
-
-#define CMC_ERRA_IF(condition, err_var, err_type, action)                      \
-  if (condition) {                                                             \
-    CMC_ERRA(err_type, action, err_var);                                       \
-    action;                                                                    \
-  }
+#define CMC_ERRRB(err_type) CMC_ERRA(err_type, return err_type;, buff->err)
 
 #define CMC_ERRRC_IF(condition, err_type)                                      \
   CMC_ERRA_IF(condition, conn->err, err_type, return err_type)
