05d66e32f48c4c303bc0162d23aad0ca674ab942
admin-else <adman.else@gmail.com>
2024-06-23 16:45:37 +0200

    add error catching in bool unpacking code

--- tests/fuzz_packet.c
+++ tests/fuzz_packet.c
@@ -4,11 +4,11 @@
 #include <stdint.h>
 #include <stdio.h>
 
-#define PACKET_TO_FUZZ cmc_packet_S2C_play_entity_properties_47
-#define PACKET_TO_FUZZ_UNPACK cmc_packet_S2C_play_entity_properties_47_unpack
-#define PACKET_TO_FUZZ_PACK cmc_packet_S2C_play_entity_properties_47_pack
-#define PACKET_TO_FUZZ_FREE cmc_packet_S2C_play_entity_properties_47_free
-#define PACKET_TO_FUZZ_VERS 47
+#define PACKET_TO_FUZZ        cmc_packet_S2C_play_chunk_data_47
+#define PACKET_TO_FUZZ_UNPACK cmc_packet_S2C_play_chunk_data_47_unpack
+#define PACKET_TO_FUZZ_PACK   cmc_packet_S2C_play_chunk_data_47_pack
+#define PACKET_TO_FUZZ_FREE   cmc_packet_S2C_play_chunk_data_47_free
+#define PACKET_TO_FUZZ_VERS   47
 
 int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
   cmc_buff *buff = cmc_buff_init(PACKET_TO_FUZZ_VERS);
@@ -16,11 +16,11 @@ int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
   PACKET_TO_FUZZ packet =
       CMC_ERRB_ABLE(PACKET_TO_FUZZ_UNPACK(buff), goto err_pre_packet;);
   cmc_buff *tmp = cmc_buff_init(PACKET_TO_FUZZ_VERS);
-  CMC_ERRB_ABLE(cmc_packet_S2C_play_entity_properties_47_pack(tmp, &packet),
+  CMC_ERRB_ABLE(PACKET_TO_FUZZ_PACK(tmp, &packet),
                 goto err_after_tmp);
   if (!cmc_buff_compare(buff, tmp)) {
     puts("not eqal!");
-    return 1;
+    goto err_after_tmp;
   }
   cmc_buff_free(tmp);
   cmc_buff_free(buff);
@@ -32,5 +32,6 @@ err_post_packet:
   PACKET_TO_FUZZ_FREE(&packet);
 err_pre_packet:
   cmc_buff_free(buff);
+  puts("failed!");
   return 1;
 }
