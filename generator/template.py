5cffe7a4a782fa13bbb035f240a5c0bf68ff96be
admin-else <adman.else@gmail.com>
2024-07-25 09:18:19 +0200

    first steps towards codegen

--- generator/template.py
+++ generator/template.py
@@ -1,23 +1,48 @@
 import os
 import jinja2
+import json
 
 BASE_TEMPLATES_PATH = "templates"
 ATTRIBUTE_FILE_SUFFIX = ".c.jinja"
 env = jinja2.Environment()
 types = {}
 
-def get(type_name, attribute, data):
-  if not types[type_name][attribute]:
-    raise FileNotFoundError(f"Attribut {attribute} was not found in {type_name}.")
-  template = env.from_string(types[type_name][attribute])
-  return template.render(data)
+contex = {}
+
+
+def get(type_name, attribute, *args, **kwargs):
+    if not types[type_name][attribute]:
+        raise FileNotFoundError(f"Attribute {attribute} was not found in {type_name}.")
+    template = env.from_string(types[type_name][attribute] + ATTRIBUTE_FILE_SUFFIX)
+    data = kwargs
+    data.update(contex)
+    for arg in args:
+        data.update(arg)
+
+    return template.render(data)
+
 
 def load_native_types():
-  for type_name in os.scandir(BASE_TEMPLATES_PATH):
-    for type_attribute in os.scandir(type_name):
-      with open(type_attribute) as f:
-        types.setdefault(type_name.name, {})
-        types[type_name.name][type_attribute.name.removesuffix(ATTRIBUTE_FILE_SUFFIX)] = f.read()
-        
+    for type_name in os.scandir(BASE_TEMPLATES_PATH):
+        for type_attribute in os.scandir(type_name):
+            with open(type_attribute) as f:
+                types.setdefault(type_name.name, {})
+                types[type_name.name][
+                    type_attribute.name.removesuffix(ATTRIBUTE_FILE_SUFFIX)
+                ] = f.read()
+
+
+def main():
+    with open("minecraft-data/data/pc/1.20.3/protocol.json", "r") as f:
+        types = json.load(f)
+        for name in types["types"]:
+            rawdata = types["types"][name]
+            if rawdata == "native":
+                continue  # TODO: Check if type is in types array
+
+            get("type", "code", rawdata)
+
+
 load_native_types()
 print(types)
+main()
--- src/heap_utils.h
+++ src/heap_utils.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "cmc/err.h"
+
 void *cmc_malloc(unsigned long len, cmc_err *err);
 void *cmc_realloc(void *in_p, unsigned long len, cmc_err *err);
 void cmc_free(void *p);
