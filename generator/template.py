4c05ff97b44fec490b283791a831d8b242c77ee8
admin-else <adman.else@gmail.com>
2024-08-02 22:06:59 +0200

    generate files

--- generator/template.py
+++ generator/template.py
@@ -12,6 +12,7 @@ template_globals = {}
 def set_template_global(k: str, v):
     global template_globals
     template_globals[k] = v
+    return ""
 
 
 def json_loadf(fname):
@@ -36,12 +37,14 @@ def get(attribute, type_name, *args, **kwargs):
     if attribute not in types[type_name]:
         raise FileNotFoundError(f"Attribute {attribute} was not found in {type_name}.")
     template = env.from_string(types[type_name][attribute])
+    print(f"Renderin {type_name}::{attribute}!")
 
     data = kwargs
     data.update(contex)
     data.update(template_globals)
 
     for arg in args:
+        print(arg)
         data.update(arg)
 
     return template.render(data)
@@ -49,9 +52,10 @@ def get(attribute, type_name, *args, **kwargs):
 
 def load_native_types():
     for type_name in os.scandir(BASE_TEMPLATES_PATH):
+        if type_name not in types:
+            types[type_name.name] = {}
         for type_attribute in os.scandir(type_name):
             with open(type_attribute) as f:
-                types.setdefault(type_name.name, {})
                 types[type_name.name][
                     type_attribute.name.removesuffix(ATTRIBUTE_FILE_SUFFIX)
                 ] = f.read()
@@ -59,27 +63,37 @@ def load_native_types():
 
 def main():
     protocol_version = 765  # TODO: parse this
-    types = json_loadf("minecraft-data/data/pc/1.20.3/protocol.json")
-    for name in types["types"]:
-        rawdata = types["types"][name]
+    proto = json_loadf("minecraft-data/data/pc/1.20.3/protocol.json")
+    for name in proto["types"]:
+        rawdata = proto["types"][name]
         if rawdata == "native":
             if name not in types:
                 print(f"native undfined! {name}")
                 exit(1)
+            print(name)
+            continue
+
+        extra_data = {
+            "data": rawdata,
+            "name": name,
+            "t": f"cmc_{protocol_version}_{name}",
+        }
+
         # t short for type will be used VERY much so a short name is nice
-        d = get(
-            "code",
-            "type",
-            {
-                "data": rawdata,
-                "name": name,
-                "t": f"cmc_{protocol_version}_{name}",
-            },
-        )  # type: ignore
-        print(d)
+        code = get("code", "type", extra_data)
+        header = get("header", "type", extra_data)
+
+        path = f"gened/{protocol_version}/{name}"
+        os.makedirs(os.path.dirname(path), exist_ok=True)
+
+        with open(path + ".c", "w") as f:
+            f.write(code)
+
+        with open(path + ".h", "w") as f:
+            f.write(header)
+
         # print(rawdata)
 
 
 load_native_types()
-print(types)
 main()
